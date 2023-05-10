b2cb3c4be1e957e6cb5ad85a9bfbc6e859556094
AMAR2330 <amarkumar30799@gmail.com>
2023-05-10 10:54:08 +0530

    feature 201 changes done by vivek

--- Application.java
+++ Application.java
@@ -20,3 +20,19 @@ document.getElementById("demo").innerHTML =
 
 </body>
 </html>
+
+<script>
+var x, y, z;  // Declare 3 variables
+x = 5;    // Assign the value 5 to x
+y = 6;    // Assign the value 6 to y
+z = x + y;  // Assign the sum of x and y to z
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";
+</script>
+
+</body>
+</html>
+
+
+
